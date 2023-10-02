c918fed21a022096f9616dd1def6012408485f34
noealvarez93 <noeliaalvareztorres@gmail.com>
2023-10-02 00:17:38 -0300

    conexion API, tiempo en Montevideo

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const apiKey = 'TU_API_KEY';
-    const ciudad = 'Nueva York';
+    const apiKey = 'e66d02b5d091801b6ef58c17b5766a48'; 
+    const ciudad = 'Montevideo,uy'; 
 
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${apiKey}&units=metric`;
 
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', function () {
             const temperatura = data.main.temp;
             const descripcion = data.weather[0].description;
 
-            document.getElementById('temperature').textContent = `Temperatura: ${temperatura}°C`;
+            document.getElementById('temperature').textContent = `${temperatura}°C`;
             document.getElementById('description').textContent = `Condición: ${descripcion}`;
         })
         .catch(error => {
